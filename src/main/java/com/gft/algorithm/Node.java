ae86ba54867813c7482f51a645b715c9ab44e981
Werlos <azws@gft.com>
2016-09-23 10:24:27 +0200

    iterator impl for simple node

--- src/main/java/com/gft/algorithm/Node.java
+++ src/main/java/com/gft/algorithm/Node.java
@@ -1,7 +1,10 @@
 package com.gft.algorithm;
 
+import java.util.Iterator;
+
 /**
  * Created by azws on 2016-09-22.
  */
 public interface Node extends Iterable<Node> {
+
 }
--- src/main/java/com/gft/algorithm/NodeImpl.java
+++ src/main/java/com/gft/algorithm/NodeImpl.java
@@ -1,6 +1,7 @@
 package com.gft.algorithm;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -12,11 +13,28 @@ public final class NodeImpl implements Node {
     private final List<Node> childrenList;
 
     public NodeImpl(Node... childrenList) {
-        this.childrenList = Arrays.asList(childrenList);
+        if (childrenList != null)
+            this.childrenList = Arrays.asList(childrenList);
+        else
+            this.childrenList = Collections.emptyList();
     }
 
     @Override
     public Iterator<Node> iterator() {
-        return childrenList.iterator();
+        return new Iterator<Node>() {
+            @Override
+            public boolean hasNext() {
+                if (!childrenList.isEmpty() && childrenList.iterator().next() != null) {
+                    return true;
+                }
+                return false;
+            }
+            @Override
+            public Node next() {
+                return childrenList.iterator().next();
+            }
+        };
     }
+
+
 }
